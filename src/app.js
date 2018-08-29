fcd40c28a8d793e373b2b1daf90cbe87b2ca2e0f
Demo User 12 <demouser12@lab-cph.ibm.com>
2018-08-29 16:57:55 +0200

    final code

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
 // contents of src/app.js
 const express = require('express')
 const app = express()
+const ShippingController = require('../src/controllers/shipping-controller')
 
 app.get('/shipping', (request, response) => {
-  response.send('It works!')
-})
+  let ctrl = new ShippingController()
 
-app.listen(3000, () => console.log('ShippingService is listening on port 3000'))
+  ctrl
+    .getItemShipping({id: request.query.itemId, type: request.query.type})
+    .then(amount => {
+      response.send({ itemId: request.query.itemId, amount: amount })
+    })
+    .catch(error => {
+      response.status(500).send({ error: error.message })
+    })
+})
+let PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`ShippingService is listening on port ${PORT}`))

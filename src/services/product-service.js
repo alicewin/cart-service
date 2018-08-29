86244e6d13897974416613db8fc15787b5ec1377
Demo User 12 <demouser12@lab-cph.ibm.com>
2018-08-29 14:51:11 +0200

    lots of changes!

--- src/services/product-service.js
+++ src/services/product-service.js
@@ -1,6 +1,16 @@
 // src/services/product-service.js
+var axios = require('axios')
+
 module.exports = {
-    getProductWeight: async function(productId) {
-      // Meanwhile it can be implemented by other developers
-    }
-};
+  getProductWeight: async function (productId) {
+    return axios
+      .get('https://mycluster.icp:8899/products/' + productId)
+      .then(response => {
+        if (response.data && !Number.isNaN(parseFloat(response.data.weightLB))) {
+          return response.data.weightLB
+        } else {
+          return Promise.reject('Invalid response object')
+        }
+      })
+  }
+}
